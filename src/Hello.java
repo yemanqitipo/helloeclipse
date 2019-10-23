6b76596f38b8fc37c7001e57dccb7faaf7fbdddf
wenmingqijingshen <wenmingqijingshen@163.com>
2019-10-24 01:04:10 +0800

    second from wmqjs linux

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello, eclipse");
 		System.out.println("first from wmqjs kubuntu");
+		System.out.println("sencond from wmqjs kubuntu");
 	}
 }
